2e1961953cef4c72cd2de105ac41655713cf320a
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2020-08-01 19:14:45 +0900

    Update notes_controller.rb new&create

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -9,9 +9,21 @@ class NotesController < ApplicationController
   end
 
   def create
+    @note = @user.notes.new(note_params)
+    if @note.save
+      redirect_to user_notes_path(@user), notice: "ノートが投稿できました"
+    else
+      @notes = @user.notes
+      flash.now[:alert] = "ノートを入力してください"
+      render :new
+    end
   end
 
   private
+
+  def note_params
+    params.require(:note).permit(:date, :title, :quotation, :effort, :achievement, :todo, :free, :memo)
+  end
 
   def set_user
     @user = User.find(params[:user_id])

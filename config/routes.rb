8eb61fb641b5d9f79e9fbfe521f83660b4989c77
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2020-08-03 08:24:46 +0900

    Update routes introductions#index

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   
   resources :users, only: :show do
     resources :notes, only: [:index, :new, :create]
+    resources :introductions, only: [:index]
   end
   
 end

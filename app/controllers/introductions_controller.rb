c4b7b7a71c3f5fd5e6a9e5d2d27d431fb2984fa8
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2020-08-03 17:51:38 +0900

    Update controller new&create

--- app/controllers/introductions_controller.rb
+++ app/controllers/introductions_controller.rb
@@ -1,17 +1,38 @@
 class IntroductionsController < ApplicationController
+  before_action :set_user
+
   def index
   end
 
   def new
+    @introduction = Introduction.new
   end
 
   def create
+    @introduction = @user.introductions.new(introduction_params)
+    if @introduction.save
+      redirect_to user_introductions_path(@user), notice: "自己紹介文が作成できました"
+    else
+      @introductions = @user.introductions
+      flash.now[:alert] = "自己紹介文を入力してください"
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def introduction_params
+    params.require(:introduction).permit(:title, :content)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
   
 end

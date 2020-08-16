6d00b440c59a23a05a9fdb7cea2eb6fb64746953
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2020-08-16 10:02:45 +0900

    background-color

--- app/assets/javascripts/options.js
+++ app/assets/javascripts/options.js
@@ -8,3 +8,21 @@ $(function () {
     return false;
   });
 });
+
+$(function () {
+  $("#default-color").on("click", function () {
+    $(".container").css("background-color", "rgb(0 0 0)");
+  });
+  $("#pop-color").on("click", function () {
+    $(".container").css("background-color", "rgb(50 50 50)");
+  });
+  $("#cute-color").on("click", function () {
+    $(".container").css("background-color", "rgb(100 100 100)");
+  });
+  $("#cadual-color").on("click", function () {
+    $(".container").css("background-color", "rgb(150 150 150)");
+  });
+  $("#impact-color").on("click", function () {
+    $(".container").css("background-color", "rgb(200 200 200)");
+  });
+});
